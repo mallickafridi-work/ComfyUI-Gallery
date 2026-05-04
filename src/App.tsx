bc64c433bf097c2093f030002aa0a460e98cafd5
Afridi Mallick <mallickafridi.work@gmail.com>
2026-05-05 00:19:18 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+export function App() {
+  return (
+    <>
+      <div className='grid grid-rows-[auto_1fr] h-screen w-screen'>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // Any request starting with /api will be forwarded to backend
+      "/api": {
+        target: "http://localhost:5000", // your Express server
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
