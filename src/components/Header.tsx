c922fe645fbfa5ff368769b47e082b5fda53e96f
Afridi Mallick <mallickafridi.work@gmail.com>
2026-05-05 00:25:18 +0530

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+  return (
+    <div className='row-start-1 row-span-1 w-full py-2 flex justify-center'>
+      <h1 className="font-bold text-3xl">ComfyUI Gallery</h1>
+    </div>
+  )
+}
+
+export default Header
